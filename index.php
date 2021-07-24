a2f7c5fbfeb5001d6666577d69f188c6de345b28
Leit040 <leit2040@gmail.com>
2021-07-25 01:06:42 +0300

    function carry added

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
 
-function getNumOfUniqueCharacters($str, $n)
+function getNumOfUniqueCharacters($str, $n):int
 {
     $count = 0;
-    $str = strtolower($str);
-    $arr = array_unique(str_split($str));
+    $arr = array_unique(str_split(strtolower($str)));
     foreach ($arr as $item) {
 
         if (substr_count($str, $item) >= $n) {
@@ -16,4 +15,37 @@ function getNumOfUniqueCharacters($str, $n)
 
 }
 
-echo getNumOfUniqueCharacters("Alabama", 3);
+//echo getNumOfUniqueCharacters("Alabama11115555222333344444222666688877779999yyyy", 4).PHP_EOL;
+
+
+
+function carry(array $arr):int{
+$count = 0;
+    $arr_str_numbers = [];
+    foreach ($arr as $item){
+        $arr_str_numbers[] = str_split($item);
+    }
+
+        $size=count($arr_str_numbers[0]);
+    $temp = 0;
+        for($i=$size;$i>=0;$i--){
+
+            if(isset($arr_str_numbers[0][$i],$arr_str_numbers[1][$i])){
+
+                if($arr_str_numbers[0][$i]+$arr_str_numbers[1][$i]+$temp>9){
+                $temp=1;
+          $count++;
+        }else{
+                    $temp = 0;
+                }
+            }
+
+
+
+        }
+    return $count;
+
+
+}
+
+echo carry([255,99999999]).PHP_EOL;
